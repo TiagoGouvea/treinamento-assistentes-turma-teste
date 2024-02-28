66b1a57ac3d9b3758607b3170d1be5c278673b90
Tiago Gouvêa <tiago@tiagogouvea.com.br>
2024-02-28 15:27:23 -0300

    Add prompt

--- index.js
+++ index.js
@@ -1,21 +1,51 @@
 import OpenAi from "openai";
 import dotenv from "dotenv";
+import prompt from 'prompt';
+import colors from '@colors/colors';
 dotenv.config();
 
 const openai = new OpenAi({apiKey: process.env.OPENAI_KEY});
 
 async function main(){
-    console.log("oi");
+    const question = await promptMessage();
+    // console.log("question",question);
+
     const completion = await openai.chat.completions.create({
         messages: [
             {
                 "role": "user",
-                "content": "Bom dia!"
+                "content": question
             }
         ],
         model: "gpt-3.5-turbo"
     });
-    console.dir(completion,{depth:null});
+    console.log(completion.choices[0].message.content);
+
+    // console.dir(completion,{depth:null});
 }
+
+
+
+
+export async function promptMessage() {
+    try {
+        prompt.message = '';
+        prompt.delimiter = '';
+        const res = await prompt.get({
+            properties: {
+                message: {
+                    description: colors.magenta('Mensagem>'),
+                },
+            },
+        });
+        if (res.message?.trim() !== '') return res.message;
+    } catch (e) {
+        if (e.message === 'canceled') {
+            process.exit();
+        }
+        console.error(e);
+    }
+}
+
 
 main().then();
