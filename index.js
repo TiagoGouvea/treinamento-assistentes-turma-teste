19c22a85be4c2949043cb5e82ad86cc8fff059da
Tiago Gouvêa <tiago@tiagogouvea.com.br>
2024-02-28 15:50:04 -0300

    Start with system

--- index.js
+++ index.js
@@ -7,26 +7,38 @@ dotenv.config();
 const openai = new OpenAi({apiKey: process.env.OPENAI_KEY});
 
 async function main(){
-    const messages = [];
+    const messages = [{
+        "role": "system",
+        "content": "Você é um assistente que ajuda a montar um plano de estudo com base em uma tecnologia." +
+            "O usuário enviar uma tecnologia, e você montará um plano contendo:" +
+            "- Conceitos fundamentais" +
+            "- Primeiros passos" +
+            "- Documentação e links uteis" +
+            "- Projeto prático" +
+            "Logo no começo da conversa, já pergunte qual tecnologia a pessoa quer aprender."
+    }];
 
     while (true){
-        const question = await promptMessage();
+        if (messages.length>1){
+            const question = await promptMessage();
 
-        messages.push({
-            "role": "user",
-            "content": question
-        });
+            messages.push({
+                "role": "user",
+                "content": question
+            });
+        }
 
         const completion = await openai.chat.completions.create({
             messages,
             model: "gpt-3.5-turbo"
         });
 
+        // conferir se a pergunta tem a palavra "X", se tiver incluir um message system com informações
+
         messages.push(completion.choices[0].message);
 
         // console.log(completion.choices[0].message.content);
-        console.log(messages);
-
+        printMessages(messages, true);
     }
 
     // console.dir(completion,{depth:null});
@@ -55,5 +67,20 @@ export async function promptMessage() {
     }
 }
 
+
+function printMessages(messages, justLast) {
+    if (justLast) messages = [messages[messages.length-1]];
+
+    messages.map((msg) => {
+        if (msg.role == 'user') {
+            console.log(colors.magenta(msg.content));
+        } else if (msg.role == 'assistant') {
+            console.log(colors.green(msg.content));
+        }
+        if (msg.metadata) {
+            console.log('🔥 metadata', msg.metadata);
+        }
+    });
+}
 
 main().then();
